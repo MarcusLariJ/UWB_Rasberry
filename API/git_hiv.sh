54176b4ab80d790f0359a868afd68563f1b27d43
MarcusLariJ <62110139+MarcusLariJ@users.noreply.github.com>
2025-05-12 02:55:22 +0200

    Update git_hiv.sh

--- API/git_hiv.sh
+++ API/git_hiv.sh
@@ -3,6 +3,8 @@ echo "Lazy git update"
 GITUSER="MarcuslariJ"
 GITPASS="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"
 REPO="https://$GITUSER:$GITPASS@github.com/$GITUSER/UWB_Rasberry.git"
+echo "deleting pesky log"
+rm log.csv
 git fetch "$REPO"
 git pull "$REPO"
 echo "Done!"
