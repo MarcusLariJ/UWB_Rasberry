298e9a013cb1a8f06faaf05f92a97479bf8a5f95
MarcusLariJ <62110139+MarcusLariJ@users.noreply.github.com>
2025-05-12 20:40:47 +0200

    Update application_twr_pdoa_tag.c

--- API/Src/examples/app_00b_twr_pdoa_tag/application_twr_pdoa_tag.c
+++ API/Src/examples/app_00b_twr_pdoa_tag/application_twr_pdoa_tag.c
@@ -27,7 +27,7 @@
 #if defined(APP_TWR_PDOA)
 
 #define DEVICE_MAX_NUM 10 // number of expected devices to communicate with
-#define FORCE_ANCHOR 0 // Force the device to be an anchor and never enter tag mode
+#define FORCE_ANCHOR 1 // Force the device to be an anchor and never enter tag mode
 #define FORCE_TAG 0 // Force the device to be a tag
 
 static void tx_done_cb(const dwt_cb_data_t *cb_data);
