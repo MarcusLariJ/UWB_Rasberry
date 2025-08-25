1b1c265e36a94f1a02b6a187432da50d87382dc9
MarcusLariJ <62110139+MarcusLariJ@users.noreply.github.com>
2025-08-25 03:30:20 +0200

    Added 3d files

--- Sim/obs_analysis.py
+++ Sim/obs_analysis.py
@@ -6,50 +6,45 @@ import control as ct
 
 dt = 0.01
 mot = mf.MotionModel(dt=dt)
-t = np.array([[0.0],[0.0]])
+t = np.array([[0.1],[0.0]])
 meas = mf.MeasModel(t=t)
 
-F = mot.A
-
 # Perform Monte Carlo simulation
-iter = 1000
+iter = 50000
 np.random.seed(0)
 rank_imu = 0
 rank_rb_a = 0
 rank_rb_b = 0
 rank_rb2 = 0
-mag = np.array([[np.pi], [2*np.pi], [100], [100], [1000], [1000], [10000], [10000], [np.pi], [10], [10]])
+mag = np.array([[np.pi], [100], [100], [20], [20], [0.03], [0.15], [0.15]])
+mag2 = np.array([[2*np.pi], [100], [100]])
 for i in range(iter):
-    x0 = (np.random.rand(11, 1)-0.5)*mag
-    x1 = (np.random.rand(11, 1)-0.5)*mag
-    x2 = (np.random.rand(11, 1)-0.5)*mag
-    x3 = (np.random.rand(11, 1)-0.5)*mag
+    x0 = (np.random.rand(8, 1)-0.5)*mag
+    u0 = (np.random.rand(3, 1)-0.5)*mag2
+    x1 = (np.random.rand(8, 1)-0.5)*mag
+    x2 = (np.random.rand(8, 1)-0.5)*mag
+    x3 = (np.random.rand(8, 1)-0.5)*mag
     
-    H_imu = meas.get_jacobian_IMU(x0)
+    F = mot.get_A_B(x0=x0, u0=u0, dt=0.1)[0].copy()
     H_rb_a = meas.get_jacobian_rb(x0, x1, t).copy() # returns reference, therefore need new copy
-    H_rb_b = meas.get_jacobian_rb(x2, x1, t).copy()
-    H_rb2 = meas.get_jacobian_rb2(x0, x3, t)
-
-    # Observability for imu update
-    O1 = ct.obsv(F, H_imu)
+    H_rb_b = meas.get_jacobian_rb(x0, x2, t).copy()
+    H_rb2 = meas.get_jacobian_rb2(x0, x3, t).copy()
 
     # Observabiliy for normal RB
-    H2 = np.vstack([H_imu, H_rb_a])
+    H2 = np.vstack([H_rb_a])
     O2 = ct.obsv(F, H2)
 
-    H3 = np.vstack([H_imu, H_rb_a, H_rb_b])
+    H3 = np.vstack([H_rb_a, H_rb_b])
     O3 = ct.obsv(F, H3)
 
     # Observability for RB2 
-    H4 = np.vstack([H_imu, H_rb2])
+    H4 = np.vstack([H_rb2])
     O4 = ct.obsv(F, H4)
 
-    rank_imu += np.linalg.matrix_rank(O1)
     rank_rb_a += np.linalg.matrix_rank(O2)
     rank_rb_b += np.linalg.matrix_rank(O3)
     rank_rb2 += np.linalg.matrix_rank(O4)
 
-print(rank_imu/iter)
 print(rank_rb_a/iter)
 print(rank_rb_b/iter)
 print(rank_rb2/iter)
